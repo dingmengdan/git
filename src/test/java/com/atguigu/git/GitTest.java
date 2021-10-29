112554a89f2856d760d403f9d78f825ab1cc9999
Momoko <Momoko@qq.com>
2021-10-29 16:28:13 +0800

    commmit test

--- src/test/java/com/atguigu/git/GitTest.java
+++ src/test/java/com/atguigu/git/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
         System.out.println("git test1");
         System.out.println("git test2");
         System.out.println("git test3");
-        System.out.println("mster test");
+        System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
