bc27deacb6bf9b624fc4ea6af6ea948771baf9b5
Momoko <Momoko@qq.com>
2021-10-29 15:21:03 +0800

    first commit

--- src/test/java/com/atguigu/git/GitTest2.java
+++ src/test/java/com/atguigu/git/GitTest2.java
@@ -1,9 +1,7 @@
 package com.atguigu.git;
 
-public class GitTest {
+public class GitTest2 {
     public static void main(String[] args) {
-        System.out.println("git test1");
         System.out.println("git test2");
-        System.out.println("git test3");
     }
 }
